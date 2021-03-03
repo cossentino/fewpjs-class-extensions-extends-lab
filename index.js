1420353e7676d6e7b62128c71a0c742379a10226
Ian Cossentino <icossentino@gmail.com>
2021-03-03 18:10:39 -0500

    commit tree b26dce3c5b116371e47a375287917d40db554049

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+
+
+class Polygon {
+  
+  constructor(sides) {
+    this.sides = sides
+  }
+
+  get countSides() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    const reducer = (accum, curr) => accum + curr
+    return this.sides.reduce(reducer)
+  }
+}
+
+
+class Triangle extends Polygon {
+
+
+  get isValid() {
+    const side1 = this.sides[0]
+    const side2 = this.sides[1]
+    const side3 = this.sides[2]
+    if (side1 + side2 <= side3) {
+      return false
+    } else if (side2 + side3 <= side1) {
+      return false
+    } else if (side1 + side3 <= side2) {
+      return false
+    }
+    return true
+  }
+}
+
+
+
+class Square extends Polygon {
+
+
+
+  get isValid() {
+    return this.sides[0] === this.sides[1] &&
+    this.sides[1] === this.sides[2] &&
+    this.sides[2] === this.sides[3]
+  }
+
+  get area() {
+    return this.sides[0] ** 2
+  }
+}
